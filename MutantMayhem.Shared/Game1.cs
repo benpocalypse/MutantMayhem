b4dc37ae86c73457aca00b8e59e45aee3f9524d7
Unknown <ben.foote@gmail.com>
2020-04-18 19:12:48 -0400

    It all starts here.
    So, the kids have really wanted to play Octogeddon on their Android Tablets ever since it first came out a couple years ago. Since we still haven't heard any movement on an official release, we decided to make our own game that is in the same style. This is the beginning of it.

---
+++ MutantMayhem.Shared/Game1.cs
@@ -0,0 +1,106 @@
+﻿#region Using Statements
+using System;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+#endregion
+
+namespace MutantMayhem.Shared
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        SpriteFont font;
+        Texture2D titleScreen;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+#if ANDROID
+            graphics.IsFullScreen = true;
+            graphics.PreferredBackBufferWidth = 800;
+            graphics.PreferredBackBufferHeight = 480;
+            graphics.SupportedOrientations = DisplayOrientation.LandscapeLeft | DisplayOrientation.LandscapeRight;
+#elif LINUX
+            graphics.IsFullScreen = false;
+            graphics.PreferredBackBufferWidth = 1920;
+            graphics.PreferredBackBufferHeight = 1080;
+#endif
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            titleScreen = Content.Load<Texture2D>("titlescreen");
+
+            //  Load our spritefont file
+            font = Content.Load<SpriteFont>("font");
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // For Mobile devices, this logic will close the Game when the Back button is pressed
+            // Exit() is obsolete on iOS
+#if !__IOS__ && !__TVOS__
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
+                Keyboard.GetState().IsKeyDown(Keys.Escape))
+            {
+                Exit();
+            }
+#endif
+            // TODO: Add your update logic here			
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            spriteBatch.Begin();
+            spriteBatch.Draw(titleScreen, new Vector2(0, 0), null, Color.White, 0f, Vector2.One, Vector2.One, SpriteEffects.None, 0);
+
+#if ANDROID
+                spriteBatch.DrawString(font, "Hello World! This is the Android project", new Vector2(100, 100), Color.Black);
+#elif LINUX
+            spriteBatch.DrawString(font, "Press any button to start!", new Vector2(850, 980), Color.Black);
+#endif
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
